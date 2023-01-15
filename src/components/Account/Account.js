dfc20cb5fdd0098075f3e8e2fb93a89b5663c858
T-erry <terrywachuka10@gmail.com>
2023-01-16 01:05:36 +0300

    added a GET request

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Search from '../Search/Search'
 import TransactionForm from '../TrasactionForm/TransactionForm'
 import TransactionList from '../TransactionList/TransactionList'
 
+
 function Account() {
+    const [transactions, setTransactions] = useState([])
+
+    useEffect(() => {
+      fetch("http://localhost:8001/transactions")
+      .then((response)=> response.json())
+      .then((data)=>{
+        setTransactions(data)
+      })
+    }, [])
   return (
     <div>
         <Search/>
         <TransactionForm/>
-        <TransactionList/>
+        <TransactionList transactions={transactions}/>
     </div>
-  )
+  );
 }
 
 export default Account
