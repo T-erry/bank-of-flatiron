e92519808d428c9f0cd7b8db8d77a74ad5a8b34b
T-erry <terrywachuka10@gmail.com>
2023-01-16 01:38:11 +0300

    added a Transaction component

---
+++ src/components/Transaction/Transaction.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Transaction({date, description, category, amount}) {
+  return (
+    <>
+    <tr>
+      <td>{date}</td>
+      <td>{description}</td>
+      <td>{category}</td>
+      <td>{amount}</td>
+      <td><button className="btn btn-primary">Delete</button></td>
+
+    </tr>
+    </>
+  )
+}
+
+export default Transaction
